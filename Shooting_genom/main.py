de33b69b654e43db55580d41cdf70c95917f0842
Hyuunjun-Bruce-Lee <hyunjun.bruce.lee@gmail.com>
2024-01-26 19:57:39 +0900

    :construction: add new game & chage file name

---
+++ Shooting_genom/main.py
@@ -0,0 +1,135 @@
+import numpy as np
+import pygame, random
+from copy import deepcopy
+from shooting_game import shooting, SCREEN_SIZE, PIXEL_SIZE
+import pickle
+from datetime import datetime
+
+class Genome():
+    def __init__(self):
+        self.fitness = 0
+        self.simulation_time = 10
+        # define weight matrix
+        hidden_layer = 10
+        self.w1 = np.random.randn(30, hidden_layer)
+        self.w2 = np.random.randn(hidden_layer, 20)
+        self.w3 = np.random.randn(20, hidden_layer)
+        self.w4 = np.random.randn(hidden_layer, 3)
+      
+    def forward(self, inputs):
+        # define foward method
+        net = np.matmul(inputs, self.w1)
+        net = self.leaky_relu(net)
+        net = np.matmul(net, self.w2)
+        net = self.leaky_relu(net)
+        net = np.matmul(net, self.w3)
+        net = self.leaky_relu(net)
+        net = np.matmul(net, self.w4)
+        net = self.softmax(net)
+        return net
+    
+    def softmax(self, x):
+        return np.exp(x) / np.sum(np.exp(x), axis=0)
+  
+    def leaky_relu(self, x):
+        return np.where(x > 0, x, x * 0.01)
+
+
+N_POPULATION = 50 # number of genoms (pool)
+N_BEST = 5 # number of genoms to keep after each generation (if n == 5, ~ Top 5)
+N_CHILDREN = 5 # number of childrens in each generation
+PROB_MUTATION = 0.4
+
+
+pygame.init()
+pygame.font.init()
+screen = pygame.display.set_mode((SCREEN_SIZE * PIXEL_SIZE, SCREEN_SIZE * PIXEL_SIZE))
+pygame.display.set_caption('shooting')
+
+genomes = [Genome() for _ in range(N_POPULATION)]
+best_genomes = None
+
+
+
+n_gen = 0
+while True:
+    time_a = datetime.now()
+
+    n_gen += 1
+  
+    for i, genome in enumerate(genomes):
+        snake = shooting(screen, genome=genome)
+        fitness, score = snake.run(genome.simulation_time)
+    
+        genome.fitness = fitness
+    
+        # print('Generation #%s, Genome #%s, Fitness: %s, Score: %s' % (n_gen, i, fitness, score))
+
+    if best_genomes is not None:
+        genomes.extend(best_genomes)
+    genomes.sort(key=lambda x: x.fitness, reverse=True)
+
+    time_b = datetime.now()
+    
+    with open('./genoms/genom.bin', 'wb') as f:
+       pickle.dump(genomes, f)
+
+    print(f'>>> Generaton #{n_gen}\n\tBest Fitness {genomes[0].fitness}\n\tsimulation time : {time_b - time_a}')
+    # print(genomes[0].w1, genomes[0].w2)
+  
+    best_genomes = deepcopy(genomes[:N_BEST])
+    
+    # create children
+    for i in range(N_CHILDREN):
+        new_genome = deepcopy(best_genomes[0])
+        # extract two genomes from best genomes for inheritance
+        a_genome = random.choice(best_genomes)
+        b_genome = random.choice(best_genomes)
+
+        # randomly inherit from extracted two genomes (extracted genomes are father and mother)
+        # created genom(children) will be configured based on its mother and father
+        # the persentage is random (mother & father)
+        cut = random.randint(0, new_genome.w1.shape[1])
+        new_genome.w1[i, :cut] = a_genome.w1[i, :cut]
+        new_genome.w1[i, cut:] = b_genome.w1[i, cut:]
+
+        cut = random.randint(0, new_genome.w2.shape[1])
+        new_genome.w2[i, :cut] = a_genome.w2[i, :cut]
+        new_genome.w2[i, cut:] = b_genome.w2[i, cut:]
+
+        cut = random.randint(0, new_genome.w3.shape[1])
+        new_genome.w3[i, :cut] = a_genome.w3[i, :cut]
+        new_genome.w3[i, cut:] = b_genome.w3[i, cut:]
+
+        cut = random.randint(0, new_genome.w4.shape[1])
+        new_genome.w4[i, :cut] = a_genome.w4[i, :cut]
+        new_genome.w4[i, cut:] = b_genome.w4[i, cut:]
+
+        new_genome.simulation_time += 2
+
+        # add created genom to best genome group (as its constructed with the ones from best genomes)
+        best_genomes.append(new_genome)
+
+
+    # mutation (add divercity)
+    # as childrens are solely based on the perents, the pool of genes are limited to the genes initialized at the begining.
+    # to add diversity(for poential oprimal weight) replace partial of childrens gene(respect to mutation probability) with random value and create aditional children with mutation
+    # this makes the algorithm not to require differentiation for training which makes it to run much faster
+    genomes = []
+    for i in range(int(N_POPULATION / (N_BEST + N_CHILDREN))):
+        for bg in best_genomes:
+            new_genome = deepcopy(bg)
+      
+            mean = 20
+            stddev = 10
+
+            if random.uniform(0, 1) < PROB_MUTATION:
+              new_genome.w1 += new_genome.w1 * np.random.normal(mean, stddev, size=(30, 10)) / 100 * np.random.randint(-1, 2, (30, 10))
+            if random.uniform(0, 1) < PROB_MUTATION:
+              new_genome.w2 += new_genome.w2 * np.random.normal(mean, stddev, size=(10, 20)) / 100 * np.random.randint(-1, 2, (10, 20))
+            if random.uniform(0, 1) < PROB_MUTATION:
+              new_genome.w3 += new_genome.w3 * np.random.normal(mean, stddev, size=(20, 10)) / 100 * np.random.randint(-1, 2, (20, 10))
+            if random.uniform(0, 1) < PROB_MUTATION:
+              new_genome.w4 += new_genome.w4 * np.random.normal(mean, stddev, size=(10, 3)) / 100 * np.random.randint(-1, 2, (10, 3))
+        
+            genomes.append(new_genome)
---
+++ Shooting_genom/shooting_game.py
@@ -0,0 +1,151 @@
+import pygame
+import random
+import numpy as np
+from copy import deepcopy
+
+FPS = 60
+SCREEN_SIZE = 30
+PIXEL_SIZE = 5
+LINE_WIDTH = 1
+
+BULET_MISS_PENALTY = 1.2
+SHOOT_REWORD = 1
+MOVE_OUT_OF_BORD_PENALTY = 0.8
+TARGET_HIT_REWORD = 10
+
+class shooting:
+    def __init__(self, screen, genome):
+        self.earned_reword = 0
+        self.screen = screen
+        self.genome = genome
+        self.target = None
+        self.bulet = list()
+        self.shooter = [round(SCREEN_SIZE/2), 3] #shooters initial location
+        self.score = 0
+        self.generate_target()
+
+    def generate_target(self):
+        flag = np.random.randint(100)
+        # generate target from left if flag is odd number else from right
+        # d stans for direction(direction for bulit to move toward)
+        x,d = (0,0) if flag%2 == 1 else (SCREEN_SIZE-1,1)
+        self.target = np.array([x,25,d])
+
+    def generate_bulet(self):
+        # generate bulet when shooter shoots(K_SPACE)
+        self.bulet.append([deepcopy(self.shooter[0]), 4])
+
+    def move_target(self):
+        # move_target according to direction
+        if self.target[-1] == 0:
+            self.target += [1,0,0]
+        else:
+            self.target -= [1,0,0]
+        
+        # if target reaches screen border regenerate target
+        if self.target[0] in [0,SCREEN_SIZE]:
+            self.generate_target()
+
+    def move_bulet(self):
+        # move bulet upward
+        if len(self.bulet) != 0:
+            self.bulet = [[i,j+1] for i,j in self.bulet]
+        for i in self.bulet:
+            if i[1] >= SCREEN_SIZE:
+                self.bulet.remove(i)
+                self.earned_reword -= BULET_MISS_PENALTY
+
+
+    def target_hit_check(self):
+        # event when bulet hits the target
+        for b in self.bulet:
+            if (self.target[0] == b[0]) and (self.target[1] == b[1]):
+                self.score += 1
+                self.earned_reword += TARGET_HIT_REWORD
+                self.generate_target()
+
+    def control(self,key):
+        # define actions in game
+        new_position = [-1,-1]
+        if key == pygame.K_LEFT:
+            new_position = np.array(self.shooter) - [1,0]
+        elif key == pygame.K_RIGHT:
+            new_position = np.array(self.shooter) + [1,0]
+        elif key == pygame.K_SPACE:
+            self.generate_bulet()
+            self.earned_reword += SHOOT_REWORD
+        
+        if new_position[0] in range(1,SCREEN_SIZE):
+            # check shooters position (doesn't move when shooter goes out of screen)
+            self.shooter = deepcopy(new_position)
+        else:
+            self.earned_reword -= MOVE_OUT_OF_BORD_PENALTY
+
+    def generate_inputs(self):
+        # genoms will get current screen info as an input
+        current_screen_info = np.zeros([SCREEN_SIZE,SCREEN_SIZE])
+        current_screen_info[self.target[0]][self.target[1]] = 1 # mark current target location as 1
+        return np.array(current_screen_info)
+
+    def run(self, simulation_time):
+        font = pygame.font.Font('/Users/hyunjun_bruce_lee/Library/Fonts/FiraCode-Bold.ttf', 20)
+        font.set_bold(True)
+        shooter_img, bulet_img, target_img = pygame.Surface((PIXEL_SIZE, PIXEL_SIZE)), pygame.Surface((PIXEL_SIZE, PIXEL_SIZE)), pygame.Surface((PIXEL_SIZE, PIXEL_SIZE))
+        shooter_img.fill((0,0,255)), bulet_img.fill((0,255,0)), target_img.fill((255,200,200))
+        clock = pygame.time.Clock()
+        flag = 0
+
+        while True:
+            if flag > simulation_time:
+                break
+            clock.tick(FPS)
+            for e in pygame.event.get():
+                # shut down
+                if e.type == pygame.QUIT:
+                    pygame.quit()
+                # moves
+                elif e.type == pygame.KEYDOWN:
+                    self.control(e.key)
+
+            if __name__ != '__main__':
+                inputs = self.generate_inputs()
+                outputs = self.genome.forward(inputs)
+                out_key = np.argmax(outputs)
+                if out_key == 0:
+                    key_event = pygame.K_SPACE
+                elif out_key == 1:
+                    key_event = pygame.K_LEFT
+                else:
+                    key_event = pygame.K_RIGHT
+            
+            self.control(key_event)
+            if flag%2 == 1:
+                self.move_target()
+            self.move_bulet()
+            self.target_hit_check()
+
+            self.screen.fill((0,0,0))
+            self.screen.blit(shooter_img, (self.shooter[0] * PIXEL_SIZE, self.shooter[1] * PIXEL_SIZE))
+            self.screen.blit(shooter_img, ((self.shooter[0]+1) * PIXEL_SIZE, self.shooter[1] * PIXEL_SIZE))
+            self.screen.blit(shooter_img, ((self.shooter[0]-1) * PIXEL_SIZE, self.shooter[1] * PIXEL_SIZE))
+            self.screen.blit(shooter_img, (self.shooter[0] * PIXEL_SIZE, (self.shooter[1] + 1) * PIXEL_SIZE))
+            self.screen.blit(target_img, (self.target[0] * PIXEL_SIZE, self.target[1]  * PIXEL_SIZE))
+            for b in self.bulet:
+                self.screen.blit(bulet_img, (b[0] * PIXEL_SIZE, b[1] * PIXEL_SIZE))
+            score_txt = font.render(str(self.score), False, (255, 255, 255))
+            self.screen.blit(score_txt, (5,5))
+            pygame.display.update()
+            flag += 1
+        return self.earned_reword, self.score
+
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.font.init()
+    s = pygame.display.set_mode((SCREEN_SIZE * PIXEL_SIZE, SCREEN_SIZE * PIXEL_SIZE))
+    pygame.display.set_caption('Shooting')
+
+    while True:
+        game = shooting(s,genome = None)
+        indicator, score = game.run()
+        print(f'indicator : {indicator}, score : {score}')
